b6ac4a879145c29eb5a96c40cf1b1aeca84439d4
David Bruce Yoon <1432282729@qq.com>
2016-11-15 19:56:53 +0800

    提交客户端代码

---
+++ Unity/UnityVsProject/UnityAnim/Assets/Scripts/RoleContoller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoleContoller : MonoBehaviour {
+
+    public AudioSource bgm;
+    Animator anim;
+    bool initPlay = true;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+    public void OnMoveRole() {
+        bgm.Play();
+        anim.SetTrigger("Dance");
+        //设置旋转播放
+        if (initPlay)
+        {
+            anim.SetLayerWeight(1, 1f);
+            initPlay = false;
+        }else {
+            anim.SetTrigger("DanceRotation");
+        }
+    }
+
+    public void OnStopRole()
+    {
+        bgm.Stop();
+        anim.SetTrigger("IdelRotation");
+        anim.SetTrigger("Idel");
+    }
+
+    void CallUnity(object obj)
+    {
+        string val = string.Format("{0}", obj);
+        Debug.Log("val=" + val);
+    }
+}
---
+++ Unity/UnityVsProject/UnityAnim/Assets/Scripts/TestController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestController : MonoBehaviour {
+
+
+    public Text text;
+
+    private int count = 0;
+
+    public void OnTest() {
+
+        count++;
+        text.text = count + "";
+    }
+
+    public void CallUnity(Object obj) {
+        text.text = obj.ToString();
+    }
+
+}
